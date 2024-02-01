2fccc3a1d8ee4de776fbb26f253bebef47c724a9
Vishnu-D-2002 <vishnulosangels@gmail.com>
2024-02-01 08:24:29 +0530

    except nav

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import Login from './components/login';
+import PasswordReset from './components/passwordreset';
+import NewPassword from './components/newpassword';
+import Active from './components/active';
+import Dashboard from './components/Dashboard';
+import Products from './components/products';
+import Ticket from './components/Ticket';
+import CreateTicket from './components/CreateTicket';
+
+
+function App() {
+  return (
+    <div>
+      <Router>
+        <div>
+          <Routes>
+            <Route path='/' element={<Login />} />
+            <Route path='/reset-password' element={<PasswordReset />} />
+            <Route path='/reset-password/new-password/:OTP' element={<NewPassword />} />
+            <Route path='/activate-account/:activationToken' element={<Active />} />
+            <Route path='/dashboard' element={<Dashboard />} />
+            <Route path='/ticket' element={<Ticket />} />
+            <Route path='create' element={<CreateTicket />} />
+            <Route path='/products' element={<Products />} />
+          </Routes>
+        </div>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
